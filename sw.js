6cfc2335f0d434d1e62dad484c89c47f4ca60527
RLM11193 <rmartin.nzrm@gmail.com>
2025-08-12 12:19:54 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,43 @@
-self.addEventListener('install', e=>{e.waitUntil(caches.open('harmonics-v26c').then(c=>c.addAll(['./','./index.html','./manifest.webmanifest'])))});self.addEventListener('fetch', e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
+const CACHE_NAME = 'gann-master-wheel-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+// Install the service worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Activate the service worker
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch files from cache first, then network
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
